6c53cdeb56f80418faac3a594398127b61762466
Niko Köser <koeserniko@gmail.com>
2023-11-11 00:16:05 +0100

    fix(buildinfo): make version string a variable

--- pkg/buildinfo/buildinfo.go
+++ pkg/buildinfo/buildinfo.go
@@ -1,8 +1,7 @@
 package buildinfo
 
-const (
-	// AppName is the name of the application
-	AppName = "dbench"
-	// Version is the version of the application. It is set by the linker.
-	Version = "unknown"
-)
+// AppName is the name of the application
+const AppName = "dbench"
+
+// Version is the version of the application. It is set by the linker.
+var Version = "unknown"

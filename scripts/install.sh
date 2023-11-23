041ff9f6eafcd05fbf906430c1ae7986d442cd8b
Niko Köser <koeserniko@gmail.com>
2023-11-23 01:12:13 +0100

    chore(release): bump up target release version

--- scripts/install.sh
+++ scripts/install.sh
@@ -486,7 +486,7 @@ main() {
 	local autocomplete_install_rcode
 
 	dbench_bin_name="dbench"
-	dbench_version="0.7.0-alpha"
+	dbench_version="0.8.0-alpha"
 	dbench_dl_ext="tar.gz"
 	dbench_base_url="https://github.com/nikoksr/dbench/releases/download"
 	prefix="${1}"
